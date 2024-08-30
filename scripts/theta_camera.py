5e6be89dd642a0f0aa7310be05e4482db916c35d
emuemuJP <k.matsumoto.0807@gmail.com>
2024-08-30 16:55:27 +0900

    add compression mode

--- scripts/theta_camera.py
+++ scripts/theta_camera.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 import cv2
+import numpy as np
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge
 
 
-def gstreamer_pipeline(mode):
-    return (
-        f"thetauvcsrc mode={mode} ! queue ! decodebin ! autovideoconvert ! video/x-raw,format=BGRx ! queue ! videoconvert ! video/x-raw,format=BGR ! queue ! appsink"
-    )
+def gstreamer_pipeline(mode, compression):
+    if compression:
+        return (
+            f"thetauvcsrc mode={mode} ! queue ! decodebin ! autovideoconvert ! video/x-raw,format=YUY2 ! queue ! videoconvert ! video/x-raw,format=BGR ! queue ! appsink"
+        )
+    else:
+        return (
+            f"thetauvcsrc mode={mode} ! queue ! decodebin ! autovideoconvert ! video/x-raw,format=xBGR ! queue ! videoconvert ! video/x-raw,format=BGR ! queue ! appsink"
+        )
 
 
 def start_node():
@@ -17,25 +23,42 @@ def start_node():
 
     # rosparam から mode を取得し、デフォルト値を "4K" に設定
     mode = rospy.get_param("~mode", "4K")
+    use_compression = rospy.get_param("~use_compression", True)
 
     # mode が "2K" または "4K" でない場合はエラーを出力し、デフォルト値 "4K" を使用
     if mode not in ["2K", "4K"]:
         rospy.logerr(f"Invalid mode '{mode}'. Using default mode '4K'.")
         mode = "4K"
 
-    src = gstreamer_pipeline(mode)
+    src = None
+    pub_theta_camera = None
+    if use_compression:
+        src = gstreamer_pipeline(mode, True)
+        pub_theta_camera = rospy.Publisher("~image/compressed", CompressedImage, queue_size=2)
+    else:
+        src = gstreamer_pipeline(mode, False)
+        pub_theta_camera = rospy.Publisher("~image", Image, queue_size=2)
     rospy.loginfo(f"Using pipeline: {src}")
 
     cap = cv2.VideoCapture(src, cv2.CAP_GSTREAMER)
-    pub_theta_camera = rospy.Publisher("~image", Image, queue_size=100)
+    
     bridge = CvBridge()
 
     while not rospy.is_shutdown():
         ret, frame = cap.read()
         try:
-            pub_theta_camera.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
+            if use_compression:
+                timestamp = rospy.Time.now()
+                compressed_msg = CompressedImage()
+                compressed_msg.header.stamp = timestamp
+                compressed_msg.format = "jpeg"
+                compressed_msg.data = np.array(
+                    cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])[1]).tobytes()
+                pub_theta_camera.publish(compressed_msg)
+            else:
+                pub_theta_camera.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
         except Exception as e:
-            # rospy.logerr(f"Error publishing image: {e}")
+            rospy.logerr(f"Error publishing image: {e}")
             pass
 
     cap.release()
